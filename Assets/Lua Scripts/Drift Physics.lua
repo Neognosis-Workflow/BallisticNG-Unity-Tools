f9e1ea185cbd164984a524b70008a073da0a2d87
Adam Chivers <bigsnake09@googlemail.com>
2024-02-29 17:47:27 +0000

    Update drift physics lua script with 2280 additions

--- Assets/Lua Scripts/Drift Physics.lua
+++ Assets/Lua Scripts/Drift Physics.lua
@@ -12,17 +12,41 @@ end
 
 -- configures a ships grip to use the drift ships grip stat
 function SetupShipGrip(ship)
-	if Race.Speedclass == ESpeedClass:Toxic then -- toxic
-		ship.Settings.AG_GRIP = 1.6
-	elseif Race.Speedclass == ESpeedClass:Apex then -- apex
-		ship.Settings.AG_GRIP = 1.8
-	elseif Race.Speedclass == ESpeedClass:Halberd then -- halberd
-		ship.Settings.AG_GRIP = 2
-	elseif Race.Speedclass == ESpeedClass:Spectre then -- spectre
-		ship.Settings.AG_GRIP = 2
-	elseif Race.Speedclass == ESpeedClass:Zen then -- zen
-		ship.Settings.AG_GRIP = 2
+
+	if Cheats.ModernPhysics == true then
+		if Race.Speedclass == ESpeedClass:Toxic then -- toxic
+			ship.Settings.AG_GRIP = 5.2
+		elseif Race.Speedclass == ESpeedClass:Apex then -- apex
+			ship.Settings.AG_GRIP = 5.465
+		elseif Race.Speedclass == ESpeedClass:Halberd then -- halberd
+			ship.Settings.AG_GRIP = 5.75
+		elseif Race.Speedclass == ESpeedClass:Spectre then -- spectre
+			ship.Settings.AG_GRIP = 6.5
+		elseif Race.Speedclass == ESpeedClass:Zen then -- zen
+			ship.Settings.AG_GRIP = 7.25
+		end
+	else
+		if Race.Speedclass == ESpeedClass:Toxic then -- toxic
+			ship.Settings.AG_GRIP = 1.6
+		elseif Race.Speedclass == ESpeedClass:Apex then -- apex
+			ship.Settings.AG_GRIP = 1.8
+		elseif Race.Speedclass == ESpeedClass:Halberd then -- halberd
+			ship.Settings.AG_GRIP = 2
+		elseif Race.Speedclass == ESpeedClass:Spectre then -- spectre
+			ship.Settings.AG_GRIP = 2
+		elseif Race.Speedclass == ESpeedClass:Zen then -- zen
+			ship.Settings.AG_GRIP = 2
+		end
 	end
+
+	-- 2280 stats
+	ship.Settings.MODERN_GRIP_AIR = 3
+	ship.Settings.MODERN_AIRBRAKE_SLIDE = 100
+	ship.Settings.MODERN_AIRBRAKE_TURN = 5
+	ship.Settings.MODERN_AIRBRAKE_GRIP = 4.75
+	ship.Settings.MODERN_AIRBRAKE_DRAG = 2
+	ship.Settings.MODERN_AIRBRAKE_GAIN = 500
+	ship.Settings.MODERN_AIRBRAKE_FALLOFF = 500
 end
 
 -- configures the AI difficulty to function better on drift tracks
