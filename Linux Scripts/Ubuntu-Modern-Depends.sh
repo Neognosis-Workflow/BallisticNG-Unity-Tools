61282486a8f4d3a0d6b37dbe97dc05967fe7e83f
Adam Chivers <bigsnake09@googlemail.com>
2026-02-27 00:58:37 +0000

    Add sh script to install missing dependencies for Unity on modern Ubuntu based Linux distros

---
+++ Linux Scripts/Ubuntu-Modern-Depends.sh
@@ -0,0 +1,5 @@
+sudo wget https://security.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.13_amd64.deb
+sudo wget https://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu70_70.1-2ubuntu1_amd64.deb
+
+sudo dpkg -i ./libssl1.0.0_1.0.2n-1ubuntu5.13_amd64.deb
+sudo dpkg -i ./libicu70_70.1-2ubuntu1_amd64.deb
